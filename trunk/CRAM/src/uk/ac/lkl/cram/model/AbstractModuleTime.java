59f795232347d5617e50284d5d2c5a2b929c1424
bernhoran@gmail.com <bernhoran@gmail.com@e356ff2a-2fe4-ad20-e176-e66f5eb57a78>
2013-08-22 15:43:33 +0000

    Add propertyChangeSupport to AbstractModuleTime

--- trunk/CRAM/src/uk/ac/lkl/cram/model/AbstractModuleTime.java
+++ trunk/CRAM/src/uk/ac/lkl/cram/model/AbstractModuleTime.java
@@ -1,23 +1,33 @@
 package uk.ac.lkl.cram.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+/**
+ * $Date$
+ * @author Bernard Horan
+ */
 public class AbstractModuleTime implements Serializable {
+    public static final String PROP_NON_WEEKLY = "non_weekly";
+    public static final String PROP_SENIOR_RATE = "senior_rate";
+    public static final String PROP_WEEKLY = "weekly";
+    
     protected static final float HOURS_PER_DAY = 7.5F;
     private static final long serialVersionUID = 1L;
     protected float non_weekly;
     protected float seniorRate; //Between 0 and 1
     protected float weekly;
+    private PropertyChangeSupport propertySupport;
 
     public AbstractModuleTime() {
-    	this.non_weekly = 0f;
-	this.weekly= 0f;
-	this.seniorRate = 0.5f;
+	this(0f, 0f, 0.5f);
     }
-    
+
     public AbstractModuleTime(float f, float g, float h) {
+	propertySupport = new PropertyChangeSupport(this);
 	this.weekly = f;
 	this.non_weekly = g;
 	this.seniorRate = h;
@@ -37,64 +47,75 @@ public class AbstractModuleTime implements Serializable {
 
     @XmlAttribute
     public void setNonWeekly(float i) {
+	float oldValue = non_weekly;
 	non_weekly = i;
+	propertySupport.firePropertyChange(PROP_NON_WEEKLY, oldValue, non_weekly);
     }
 
     @XmlAttribute
     public void setSeniorRate(float i) {
-	if (seniorRate > 1) {
+	if (i > 1) {
 	    throw new RuntimeException("Senior Rate should be between 0 and 1, not " + i);
 	}
+	float oldValue = seniorRate;
 	seniorRate = i;
+	propertySupport.firePropertyChange(PROP_SENIOR_RATE, oldValue, seniorRate);
     }
 
     @XmlAttribute
     public void setWeekly(float i) {
+	float oldValue = weekly;
 	weekly = i;
+	propertySupport.firePropertyChange(PROP_WEEKLY, oldValue, weekly);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+	propertySupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+	propertySupport.removePropertyChangeListener(listener);
     }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Float.floatToIntBits(non_weekly);
-		result = prime * result + Float.floatToIntBits(seniorRate);
-		result = prime * result + Float.floatToIntBits(weekly);
-		return result;
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + Float.floatToIntBits(non_weekly);
+	result = prime * result + Float.floatToIntBits(seniorRate);
+	result = prime * result + Float.floatToIntBits(weekly);
+	return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj) {
+	    return true;
 	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof AbstractModuleTime)) {
-			return false;
-		}
-		AbstractModuleTime other = (AbstractModuleTime) obj;
-		if (Float.floatToIntBits(non_weekly) != Float
-				.floatToIntBits(other.non_weekly)) {
-			return false;
-		}
-		if (Float.floatToIntBits(seniorRate) != Float
-				.floatToIntBits(other.seniorRate)) {
-			return false;
-		}
-		if (Float.floatToIntBits(weekly) != Float.floatToIntBits(other.weekly)) {
-			return false;
-		}
-		return true;
+	if (obj == null) {
+	    return false;
 	}
-
-    
-
+	if (!(obj instanceof AbstractModuleTime)) {
+	    return false;
+	}
+	AbstractModuleTime other = (AbstractModuleTime) obj;
+	if (Float.floatToIntBits(non_weekly) != Float
+		.floatToIntBits(other.non_weekly)) {
+	    return false;
+	}
+	if (Float.floatToIntBits(seniorRate) != Float
+		.floatToIntBits(other.seniorRate)) {
+	    return false;
+	}
+	if (Float.floatToIntBits(weekly) != Float.floatToIntBits(other.weekly)) {
+	    return false;
+	}
+	return true;
+    }
 }
