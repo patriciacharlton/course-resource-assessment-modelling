d3b4db7278b4009b73210c4f00532ea26d372d99
bernard@ellesley.com <bernard@ellesley.com@e356ff2a-2fe4-ad20-e176-e66f5eb57a78>
2014-02-11 16:57:33 +0000

    Improve tooltips on charts.

--- trunk/CRAM-UI/src/uk/ac/lkl/cram/ui/LearningExperienceChartFactory.java
+++ trunk/CRAM-UI/src/uk/ac/lkl/cram/ui/LearningExperienceChartFactory.java
@@ -6,6 +6,8 @@ import java.awt.Paint;
 import java.beans.IndexedPropertyChangeEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.text.NumberFormat;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
@@ -15,6 +17,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.labels.StandardCategoryToolTipGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.StackedBarRenderer;
@@ -39,7 +42,8 @@ import uk.ac.lkl.cram.model.TLActivity;
  */
 public class LearningExperienceChartFactory {   
     private static final Logger LOGGER = Logger.getLogger(LearningExperienceChartFactory.class.getName());
-
+    private static final NumberFormat FORMATTER = NumberFormat.getPercentInstance();
+    
     private static final Color ONE_SIZE_FITS_ALL_COLOR = Color.WHITE;
     private static final Color PERSONALISED_COLOR = new Color(128,128,128);
     private static final Color SOCIAL_COLOR = new Color(42,42,42);
@@ -124,6 +128,25 @@ public class LearningExperienceChartFactory {
 	sbRenderer.setSeriesPaint(0, PERSONALISED_COLOR);
 	sbRenderer.setSeriesPaint(1, SOCIAL_COLOR);
 	sbRenderer.setSeriesPaint(2, ONE_SIZE_FITS_ALL_COLOR);
+        sbRenderer.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator() {
+
+            @Override
+            public String generateToolTip(CategoryDataset cd, int row, int column) {
+                //Only interested in row, as there's only one column
+                //TODO--really inefficient
+                @SuppressWarnings("unchecked")
+                List<Comparable> rows = cd.getRowKeys();
+                Comparable columnKey = cd.getColumnKey(column);
+                int total = 0;
+                for (Comparable comparable : rows) {
+                    total += cd.getValue(comparable, columnKey).intValue();
+                }
+
+                Comparable rowKey = cd.getRowKey(row);
+                float value = cd.getValue(rowKey, columnKey).floatValue();
+                return cd.getRowKey(row) + " (" + FORMATTER.format(value/total) + ")";
+            }
+        });
 	CategoryAxis categoryAxis = plot.getDomainAxis();
 	//categoryAxis.setCategoryMargin(0.5D);//Makes no difference
 	categoryAxis.setVisible(false);
