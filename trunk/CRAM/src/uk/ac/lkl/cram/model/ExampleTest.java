440ee3647f64cdfc952e3f901938983bcbe70c8b
bernard@ellesley.com <bernard@ellesley.com@e356ff2a-2fe4-ad20-e176-e66f5eb57a78>
2014-03-17 16:35:02 +0000

    Added Apache licence to model classes.

--- trunk/CRAM/src/uk/ac/lkl/cram/model/ExampleTest.java
+++ trunk/CRAM/src/uk/ac/lkl/cram/model/ExampleTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 London Knowledge Lab, Institute of Education.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.lkl.cram.model;
 
 import java.io.IOException;
@@ -8,7 +23,6 @@ import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBException;
-import uk.ac.lkl.cram.model.Run;
 import uk.ac.lkl.cram.model.io.ModuleExporter;
 import uk.ac.lkl.cram.model.io.ModuleMarshaller;
 
