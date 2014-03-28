372157f25983e4bacba4c661b3d968fa1378ef0d
bernard <bernard@ellesley.com>
2014-03-28 09:32:50 +0000

    Resolve issue 7: poor layout of learning experience chart. Also added javadoc and code comments.

--- CRAM-UI/src/uk/ac/lkl/cram/ui/LearningExperienceChartFactory.java
+++ CRAM-UI/src/uk/ac/lkl/cram/ui/LearningExperienceChartFactory.java
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright 2014 London Knowledge Lab, Institute of Education.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.lkl.cram.ui;
 
 import java.awt.Color;
@@ -36,20 +50,28 @@ import uk.ac.lkl.cram.model.TLALineItem;
 import uk.ac.lkl.cram.model.TLActivity;
 
 /**
- * $Date$
- * $Revision$
+ * This class is a factory that produces an instance of class ChartPanel. The ChartPanel 
+ * presents a display of the learning experiences for a module, rendered in the form of 
+ * a stacked bar chart. The factory is responsible for setting up all the parameters of
+ * the chart, including the underlying dataset, legend, colours, and so on.
+ * @see org.jfree.chart.ChartPanel
+ * @version $Revision$
  * @author Bernard Horan
  */
+//$Date$
 public class LearningExperienceChartFactory {   
     private static final Logger LOGGER = Logger.getLogger(LearningExperienceChartFactory.class.getName());
+    //The number formatter for the tooltips
     private static final NumberFormat FORMATTER = NumberFormat.getPercentInstance();
     
+    //The colours for the bars
     private static final Color ONE_SIZE_FITS_ALL_COLOR = Color.WHITE;
     private static final Color PERSONALISED_COLOR = new Color(128,128,128);
     private static final Color SOCIAL_COLOR = new Color(42,42,42);
 
     /**
-     * @param args the command line arguments
+     * For testing purposes only
+     * @param args the command line arguments (ignored)
      */
     public static void main(String[] args) {
 	JFrame frame = new JFrame("Learning Experience Test");
@@ -59,17 +81,33 @@ public class LearningExperienceChartFactory {
 	frame.setVisible(true);
     }
     
+    /**
+     * Create a new instance of ChartPanel from the module provided. 
+     * @param m the module containing teaching-learning activities
+     * @return a chart panel containing a stacked bar chart on the learning experiences of the module's activities
+     */
     public static ChartPanel createChartPanel(Module m) {
+        //Create the dataset
 	CategoryDataset dataset = createDataSet(m);
+        //Create the chart from the dataset
 	JFreeChart chart = createChart(dataset);
+        //Create the chartpanel to render the chart
 	ChartPanel chartPanel = new ChartPanel(chart);
 	return chartPanel;
     }
 
+    /**
+     * Create a dataset from the module
+     * @param module the module containing the teaching-learning activities
+     * @return a category dataset that is used to produce a stacked bar chart
+     */
     private static CategoryDataset createDataSet(final Module module) {
-	final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-	populateDataset(dataset, module);
-	final PropertyChangeListener learningExperienceListener = new PropertyChangeListener() {
+	//Create the dataset to hold the data
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	//Populate the dataset with the data
+        populateDataset(dataset, module);
+        //Create a listener, which repopulates the dataset when anything changes
+        final PropertyChangeListener learningExperienceListener = new PropertyChangeListener() {
 
 	    @Override
 	    public void propertyChange(PropertyChangeEvent pce) {
@@ -78,56 +116,82 @@ public class LearningExperienceChartFactory {
 	    }
 	};
 	
-	for (TLALineItem lineItem : module.getTLALineItems()) {
+	//Add the listener to each of the module's tlaLineItems, as well as to each
+        //tlaLineItem's activity
+        //This means that whenever a tlaLineItem changes, or its activity changes, the listener is triggered
+        //Causing the dataset to be repopulated
+        for (TLALineItem lineItem : module.getTLALineItems()) {
 	    //LOGGER.info("adding listeners to : " + lineItem.getName());
 	    lineItem.getActivity().addPropertyChangeListener(TLActivity.PROP_LEARNING_EXPERIENCE, learningExperienceListener);
 	    lineItem.addPropertyChangeListener(learningExperienceListener);
 	}
+        //Add a listener to the module, listening for changes where a tlaLineItem is added or removed
 	module.addPropertyChangeListener(Module.PROP_TLA_LINEITEM, new PropertyChangeListener() {
-
 	    @Override
 	    public void propertyChange(PropertyChangeEvent pce) {
+		//A tlaLineItem has been added or removed
 		if (pce instanceof IndexedPropertyChangeEvent) {
-		    LOGGER.info("indexed change: " + pce);
+		    //LOGGER.info("indexed change: " + pce);
 		    if (pce.getOldValue() != null) {
 			//This has been removed
 			TLALineItem lineItem = (TLALineItem) pce.getOldValue();
-			LOGGER.info("removing listeners from: " + lineItem.getName());
+			//So remove the listener from it and its activity
+			//LOGGER.info("removing listeners from: " + lineItem.getName());
 			lineItem.getActivity().removePropertyChangeListener(TLActivity.PROP_LEARNING_EXPERIENCE, learningExperienceListener);
                         lineItem.removePropertyChangeListener(learningExperienceListener);
 		    }
 		    if (pce.getNewValue() != null) {
 			//This has been added
 			TLALineItem lineItem = (TLALineItem) pce.getNewValue();
-			LOGGER.info("adding listeners to: " + lineItem);
+			//So add a listener to it and its activity
+			//LOGGER.info("adding listeners to: " + lineItem);
 			lineItem.getActivity().addPropertyChangeListener(TLActivity.PROP_LEARNING_EXPERIENCE, learningExperienceListener);
                         lineItem.addPropertyChangeListener(learningExperienceListener);
 		    }
 		}
+		//Assume the dataset is now out of date, so repopulate it
 		populateDataset(dataset, module);
 	    }
 	});
 	return dataset;
     }
 
+    /**
+     * Create a chart from the provide category dataset
+     * @param dataset a category data set populated with the learning experiences for the module
+     * @return a Chart that can be rendered in a ChartPanel
+     */
     private static JFreeChart createChart(CategoryDataset dataset) {
+        //Create a horizontal stacked bar chart from the chart factory, with no title, no axis labels, a legend, tooltips but no URLs
+        JFreeChart chart = ChartFactory.createStackedBarChart(null, null, null, dataset, PlotOrientation.HORIZONTAL, true, true, false);
+        //Set the background colour of the chart
 	Paint backgroundPaint = UIManager.getColor("InternalFrame.background");
-	JFreeChart chart = ChartFactory.createStackedBarChart(null, null, null, dataset, PlotOrientation.HORIZONTAL, true, true, false);
 	chart.setBackgroundPaint(backgroundPaint);
+	//Get the plot from the chart
 	CategoryPlot plot = (CategoryPlot) chart.getPlot();
+	//Set the background colour of the plot to be the same as the chart
 	plot.setBackgroundPaint(backgroundPaint);
-	plot.setOutlineVisible(false);
-	plot.setInsets(RectangleInsets.ZERO_INSETS); //Keep
-	plot.setAxisOffset(RectangleInsets.ZERO_INSETS); //Keep
+	//No outline around the bars
+        plot.setOutlineVisible(false);
+        //Remove offsets from the plot
+	plot.setInsets(RectangleInsets.ZERO_INSETS); 
+	plot.setAxisOffset(RectangleInsets.ZERO_INSETS); 
+        //Hide the range lines
 	plot.setRangeGridlinesVisible(false);
+        //Get the renderer for the plot
 	StackedBarRenderer sbRenderer = (StackedBarRenderer) plot.getRenderer();
-	sbRenderer.setBarPainter(new StandardBarPainter());
+	//Set the painter for the renderer (nothing fancy)
+        sbRenderer.setBarPainter(new StandardBarPainter());
 	//sbRenderer.setItemMargin(0.5); //Makes no difference
-	//sbRenderer.setMaximumBarWidth(0.25); //reduces width of bar as proportion of overall width
+	//reduces width of bar as proportion of overall width
+        sbRenderer.setMaximumBarWidth(0.5); 
+        //Render the bars as percentages
 	sbRenderer.setRenderAsPercentages(true);
+        //Set the colours for the bars
 	sbRenderer.setSeriesPaint(0, PERSONALISED_COLOR);
 	sbRenderer.setSeriesPaint(1, SOCIAL_COLOR);
 	sbRenderer.setSeriesPaint(2, ONE_SIZE_FITS_ALL_COLOR);
+        //Set the tooltips to render percentages
         sbRenderer.setBaseToolTipGenerator(new StandardCategoryToolTipGenerator() {
 
             @Override
@@ -137,34 +201,52 @@ public class LearningExperienceChartFactory {
                 @SuppressWarnings("unchecked")
                 List<Comparable> rows = cd.getRowKeys();
                 Comparable columnKey = cd.getColumnKey(column);
+                //Sum running total
                 int total = 0;
                 for (Comparable comparable : rows) {
                     total += cd.getValue(comparable, columnKey).intValue();
                 }
-
+                //Get the value for the row (in our case the learning type)
                 Comparable rowKey = cd.getRowKey(row);
                 float value = cd.getValue(rowKey, columnKey).floatValue();
-                return cd.getRowKey(row) + " (" + FORMATTER.format(value/total) + ")";
+                //The tooltip is the value of the learning type divided by the total, expressed as a percentage
+                return cd.getRowKey(row) + " (" + FORMATTER.format(value / total) + ")";
             }
         });
+        //Hide both axes
 	CategoryAxis categoryAxis = plot.getDomainAxis();
 	//categoryAxis.setCategoryMargin(0.5D);//Makes no difference
 	categoryAxis.setVisible(false);
 	NumberAxis numberAxis = (NumberAxis) plot.getRangeAxis();
 	numberAxis.setVisible(false);
+        //Get the legend
 	LegendTitle legend = chart.getLegend();
+        //Set the font of the legend to be the same as the platform
 	legend.setItemFont(UIManager.getFont("Label.font"));
+        //Set the background colour of the legend to be the same as the platform
 	legend.setBackgroundPaint(backgroundPaint);
+        //No frame around the legend
 	legend.setFrame(BlockBorder.NONE);
+        //Locate the legend to the right of the chart
 	legend.setPosition(RectangleEdge.RIGHT);
 	return chart;
     }
 
+    /**
+     * Populate the dataset with the values from the module.
+     * @param dataset the dataset to be populated
+     * @param m the module containing the learning types 
+     */
     private static void populateDataset(DefaultCategoryDataset dataset, Module m) {
-	float personalised = 0, social = 0, oneSizeForAll = 0;
+	//Create running totals
+        float personalised = 0, social = 0, oneSizeForAll = 0;
+        //Enumerate all the tlaLineItems
 	for (TLALineItem lineItem : m.getTLALineItems()) {
+            //get the activity for each line item
 	    TLActivity tla = lineItem.getActivity();
+            //Get the number of hours spent on this activity
 	    float total = lineItem.getTotalLearnerHourCount(m);
+            //Dependding on the learning type, update the corresponding running total
 	    switch (tla.getLearningExperience()) {
 		case PERSONALISED: {
 		    personalised += (total * 100);
@@ -180,6 +262,7 @@ public class LearningExperienceChartFactory {
 		}
 	    }
 	}
+        //Set the values in the dataset
 	dataset.setValue(personalised, "Personalised", "Learning Experience");
 	dataset.setValue(social, "Social", "Learning Experience");
 	dataset.setValue(oneSizeForAll, "Same for All", "Learning Experience");
